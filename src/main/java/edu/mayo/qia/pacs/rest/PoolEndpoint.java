a164b510da5043ae455aca3d7bc5cc439af89c3d
Xiaojiang Yang <xjyangxj@gmail.com>
2017-07-14 16:12:54 -0400

    Removed CacheCleaner class.

--- src/main/java/edu/mayo/qia/pacs/rest/PoolEndpoint.java
+++ src/main/java/edu/mayo/qia/pacs/rest/PoolEndpoint.java
@@ -1,22 +1,22 @@
 package edu.mayo.qia.pacs.rest;
 
-import edu.mayo.qia.pacs.job.CacheCleaner;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.sun.jersey.api.client.ClientResponse.Status;
+import edu.mayo.qia.pacs.components.MoveRequest;
+import edu.mayo.qia.pacs.components.Pool;
+import edu.mayo.qia.pacs.components.PoolManager;
+import edu.mayo.qia.pacs.components.PoolContainer;
+import edu.mayo.qia.pacs.components.Script;
+import edu.mayo.qia.pacs.components.User;
+import edu.mayo.qia.pacs.components.Group;
+import edu.mayo.qia.pacs.components.GroupRole;
+import edu.mayo.qia.pacs.db.GroupDAO;
+import edu.mayo.qia.pacs.db.GroupRoleDAO;
+import edu.mayo.qia.pacs.db.UserDAO;
 import io.dropwizard.hibernate.UnitOfWork;
-
-import java.io.File;
-import java.util.List;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 import org.apache.log4j.Logger;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.subject.Subject;
@@ -28,23 +28,20 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.sun.jersey.api.client.ClientResponse.Status;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.File;
+import java.util.List;
 
-import edu.mayo.qia.pacs.components.Group;
-import edu.mayo.qia.pacs.components.GroupRole;
-import edu.mayo.qia.pacs.components.MoveRequest;
-import edu.mayo.qia.pacs.components.Pool;
-import edu.mayo.qia.pacs.components.PoolContainer;
-import edu.mayo.qia.pacs.components.PoolManager;
-import edu.mayo.qia.pacs.components.Script;
-import edu.mayo.qia.pacs.components.User;
-import edu.mayo.qia.pacs.db.GroupDAO;
-import edu.mayo.qia.pacs.db.GroupRoleDAO;
-import edu.mayo.qia.pacs.db.UserDAO;
+//import edu.mayo.qia.pacs.components.*;
 
 @Component
 @Path("/pool")
@@ -151,8 +148,8 @@ public class PoolEndpoint extends Endpoint {
     for (int studyKey : request.studyKeys) {
       ObjectNode studyResult = records.addObject();
       try {
-        List<String> filePaths = template.queryForList("select FilePath from INSTANCE, SERIES, STUDY where INSTANCE.SeriesKey = SERIES.SeriesKey and SERIES.StudyKey = STUDY.StudyKey and STUDY.PoolKey = ? and STUDY.StudyKey = ?", new Object[] { id,
-            studyKey }, String.class);
+        List<String> filePaths = template.queryForList("select FilePath from INSTANCE, SERIES, STUDY where INSTANCE.SeriesKey = SERIES.SeriesKey and SERIES.StudyKey = STUDY.StudyKey and STUDY.PoolKey = ? and STUDY.StudyKey = ?",
+            new Object[] { id, studyKey }, String.class);
         for (String file : filePaths) {
           destinationContainer.importFromPool(new File(container.getPoolDirectory(), file));
         }
@@ -319,9 +316,6 @@ public class PoolEndpoint extends Endpoint {
     gr.isPoolAdmin = true;
     groupRoleDAO.create(gr);
 
-    // Remove current user from authorizationCache, so that user permission will be re-fetched next time whenever needed:
-    CacheCleaner.cleanUser(subject.getPrincipal().toString());
-
     return Response.ok(pool).build();
   }
 
@@ -366,9 +360,6 @@ public class PoolEndpoint extends Endpoint {
     // Delete
     // session.delete(pool);
     poolManager.deletePool(pool);
-
-    // Remove current user from authorizationCache, so that user permission will be re-fetched next time whenever needed:
-    CacheCleaner.cleanUser(subject.getPrincipal().toString());
 
     return Response.ok().build();
   }
